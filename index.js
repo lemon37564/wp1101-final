b84ca9b652672681289683b50ac7e1ae8ace851d
lemon37564 <stonydragon0701@gmail.com>
2021-12-17 15:18:45 +0800

    padding

--- index.js
+++ index.js
@@ -80,11 +80,11 @@ async function getRank(strength) {
 
         tmp +=
           "<th>" +
-          date.getFullYear() +
+          String(date.getFullYear()) +
           "/" +
-          String(date.getMonth() + 1) +
+          String(date.getMonth() + 1).padStart(2, "0") +
           "/" +
-          date.getDate() +
+          String(date.getDate()).padStart(2, "0") +
           "</th>";
         tmp += "</tr>";
         rank += 1;
