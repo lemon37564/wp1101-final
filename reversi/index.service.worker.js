df9fea1ca7ec5eab74349deb67930270237513ed
lemon37564 <stonydragon0701@gmail.com>
2022-03-27 13:42:39 +0800

    update game version

--- reversi/index.service.worker.js
+++ reversi/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1647668166|75053993";
+const CACHE_VERSION = "1648359685|2274400662";
 const CACHE_PREFIX = "reversi3D-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
