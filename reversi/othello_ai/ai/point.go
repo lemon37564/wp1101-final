5bc4371a322f68d984693442db475a1c6c2f1faa
lemon37564 <stonydragon0701@gmail.com>
2022-03-27 13:01:46 +0800

    remove go ai

--- reversi/othello_ai/ai/point.go
+++
@@ -1,9 +0,0 @@
-package ai
-
-type point struct {
-	x, y int
-}
-
-func (p point) String() string {
-	return string(rune('A'+p.y)) + string(rune('a'+p.x))
-}
--- reversi/othello_ai/go.mod
+++
@@ -1,5 +0,0 @@
-module reversi
-
-go 1.17
-
-require github.com/pkg/profile v1.6.0 // indirect
